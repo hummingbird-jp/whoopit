e2cd7ceba87fa1ca3c253da23250b2e83b3a199c
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2021-10-22 12:51:24 +0900

    build(flutter packages): upgrade packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gtk_flutter/components/application_state.dart';
@@ -5,7 +6,9 @@ import 'package:gtk_flutter/constants.dart';
 import 'package:gtk_flutter/pages/splash_page.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     ChangeNotifierProvider(
       create: (context) => ApplicationState(),
