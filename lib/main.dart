a9ff9de6bc835e0c120cce16a9bfe059e8f69667
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2021-10-27 23:47:29 +0900

    revert(app check): restore App Check

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -8,7 +9,8 @@ import 'package:whoopit/pages/tabs_page.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-
+  await FirebaseAppCheck.instance
+      .activate(webRecaptchaSiteKey: 'recaptcha-v3-site-key');
   runApp(
     ProviderScope(
       child: Whoopit(),
